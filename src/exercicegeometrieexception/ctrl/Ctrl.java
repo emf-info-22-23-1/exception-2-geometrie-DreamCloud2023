6e51eb9ee645c61f7b85bc67e74b1e0aa751116f
perliniN <perliniN@host.docker.internal>
2024-03-20 13:23:35 +0100

    Exercice avancement

--- src/exercicegeometrieexception/ctrl/Ctrl.java
+++ src/exercicegeometrieexception/ctrl/Ctrl.java
@@ -11,25 +11,46 @@ public class Ctrl implements ICtrlIhm {
     public void demarrer() {
         refIhm.demarrer();
     }
-    
+
     @Override
-    public void selectCalcRadius( String value ) {
-        double valueDouble = Double.parseDouble( value );
-        refIhm.afficheResultatCircle( String.valueOf( refWorker.calcRadiusCircleFromArea( valueDouble ) ) );
+    public void selectCalcRadius(String value) {
+
+        try {
+            double valueDouble = Double.parseDouble(value);
+            refIhm.afficheResultatCircle(String.valueOf(refWorker.calcRadiusCircleFromArea(valueDouble)));
+        } catch (NumberFormatException ex) {
+
+            System.out.println("-------------------------------------");
+            System.out.println("Il y a eu une exception");
+            System.out.println("Le msg est " + ex.getMessage());
+            System.out.println("-------------------------------------");
+            System.out.println("-------------------------------------");
+        }
     }
 
     @Override
-    public void selectCalcLargeur( String valueArea, String valueLongueur ) {
-        double valueAreaDouble = Double.parseDouble( valueArea );
-        double valueLongueurDouble = Double.parseDouble( valueLongueur );
-        refIhm.afficheResultatRectangle( String.valueOf( refWorker.calcLargeurFromRectangle( valueAreaDouble, valueLongueurDouble ) ) );
+    public void selectCalcLargeur(String valueArea, String valueLongueur) {
+
+        try {
+            double valueAreaDouble = Double.parseDouble(valueArea);
+            double valueLongueurDouble = Double.parseDouble(valueLongueur);
+            refIhm.afficheResultatRectangle(String.valueOf(refWorker.calcLargeurFromRectangle(valueAreaDouble, valueLongueurDouble)));
+        }
+         catch (NumberFormatException ex) {
+
+            System.out.println("-------------------------------------");
+            System.out.println("Il y a eu une exception");
+            System.out.println("Le msg est " + ex.getMessage());
+            System.out.println("-------------------------------------");
+            System.out.println("-------------------------------------");
+        }
     }
 
-    public void setRefIhm( IIhmCtrl refIhm ) {
+    public void setRefIhm(IIhmCtrl refIhm) {
         this.refIhm = refIhm;
     }
 
-    public void setRefWorker( IWrkCtrl refWorker ) {
+    public void setRefWorker(IWrkCtrl refWorker) {
         this.refWorker = refWorker;
     }
 
